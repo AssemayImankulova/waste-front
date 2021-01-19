d112b9e149e09624c20f7960d80bb154b5b60647
AssemayImankulova <49196674+AssemayImankulova@users.noreply.github.com>
2021-01-19 18:51:57 +0600

    Create EditSite.vue
    add component EditSite

---
+++ src/views/EditSite.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <h1>Edit Page</h1>
+      <form>
+        <div class="form-group">
+            <label>siteName</label>
+            <input  class="form-control"  placeholder="enter siteName">
+        </div>
+        <div class="form-group">
+            <label>siteComments</label>
+            <textarea class="form-control" rows="3" placeholder="enter siteComments"></textarea>
+        </div>
+       </form>
+       <button class="btn btn-outline-primary">Edit</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
