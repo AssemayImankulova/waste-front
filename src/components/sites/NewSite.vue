89e9d256e72d1ef85ffb9852e9fcd7758b8e52a7
AssemayImankulova <49196674+AssemayImankulova@users.noreply.github.com>
2021-01-19 20:33:35 +0600

    Update NewSite.vue

--- src/components/sites/NewSite.vue
+++ src/components/sites/NewSite.vue
@@ -16,63 +16,9 @@
 </template>
 
 <script>
-// import gql from "graphql-tag"
-// const AllPosts=gql`query (
-//     $options: PageQueryOptions
-//   ) {
-//     posts(options: $options) {
-//       data {
-//         id
-//         title
-//         body
-//       }
-//     }
-//   }`
+
 export default {
     name:'NewSite',
-    methods:{
-  //    async addNewPost(){
-  //         this.isLoading=true;
-  //         const result = await this.$apollo
-  //         .mutate ({
-  //           mutation: gql`mutation (
-  //             $input: CreatePostInput!
-  //           ) {
-  //             createPost(input: $input) {
-  //               id
-  //               title
-  //               body
-  //             }
-  //           }`,
-  //           variables: {
-  //             input:{
-  //               title: this.newPost.title,
-  //               body: this.newPost.body
-  //             }
-  //           },
-  //            update: (store, { data: { createPost } }) => {
-  //           // // Read the data from our cache for this query.
-  //            const data = store.readQuery({ query: AllPosts })
-  //           // // Add our tag from the mutation to the end
-  //             data.posts.data.push(createPost)
-  //           // // Write our data back to the cache.
-  //             store.writeQuery({ query:AllPosts, data })
-  //          }  
-
-  //         })
-  //         //  this.isLoading=true;
-  //           .then( response => {
-  //              this.isLoading=false;
-  //             this.$router.push("/sites");
-  //              console.log(response)
-  //           })
-
-
-  //          console.log("Data:", result);
-
-  //      },
-     }
-
 }
 
 </script>
