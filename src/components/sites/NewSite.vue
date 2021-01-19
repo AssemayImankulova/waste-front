73062c43e087c0ffdaa07419d11b0d6dd391f134
AssemayImankulova <49196674+AssemayImankulova@users.noreply.github.com>
2021-01-19 19:55:39 +0600

    Update NewSite.vue

--- src/components/sites/NewSite.vue
+++ src/components/sites/NewSite.vue
@@ -30,74 +30,49 @@
 //   }`
 export default {
     name:'NewSite',
-    data: () => ({
-    // newPost:{
-    //  title:"",
-    //   body:""   
-    // },
-    // getPostByIdQuery:`query($id: ID!) {
-    //   post(id: $id) {
-    //     id
-    //     title
-    //     body
-    //   }
-    // }`,
-    // isLoading:false,
-    // updatePostMutation:`mutation (
-    //   $id: ID!,
-    //   $input: UpdatePostInput!
-    // ) {
-    //   updatePost(id: $id, input: $input) {
-    //     id
-    //     title
-    //     body
-    //   }
-    // }`,
-    
-  }),
-  methods:{
-//    async addNewPost(){
-//         this.isLoading=true;
-//         const result = await this.$apollo
-//         .mutate ({
-//           mutation: gql`mutation (
-//             $input: CreatePostInput!
-//           ) {
-//             createPost(input: $input) {
-//               id
-//               title
-//               body
-//             }
-//           }`,
-//           variables: {
-//             input:{
-//               title: this.newPost.title,
-//               body: this.newPost.body
-//             }
-//           },
-//            update: (store, { data: { createPost } }) => {
-//           // // Read the data from our cache for this query.
-//            const data = store.readQuery({ query: AllPosts })
-//           // // Add our tag from the mutation to the end
-//             data.posts.data.push(createPost)
-//           // // Write our data back to the cache.
-//             store.writeQuery({ query:AllPosts, data })
-//          }  
-          
-//         })
-//         //  this.isLoading=true;
-//           .then( response => {
-//              this.isLoading=false;
-//             this.$router.push("/sites");
-//              console.log(response)
-//           })
+    methods:{
+  //    async addNewPost(){
+  //         this.isLoading=true;
+  //         const result = await this.$apollo
+  //         .mutate ({
+  //           mutation: gql`mutation (
+  //             $input: CreatePostInput!
+  //           ) {
+  //             createPost(input: $input) {
+  //               id
+  //               title
+  //               body
+  //             }
+  //           }`,
+  //           variables: {
+  //             input:{
+  //               title: this.newPost.title,
+  //               body: this.newPost.body
+  //             }
+  //           },
+  //            update: (store, { data: { createPost } }) => {
+  //           // // Read the data from our cache for this query.
+  //            const data = store.readQuery({ query: AllPosts })
+  //           // // Add our tag from the mutation to the end
+  //             data.posts.data.push(createPost)
+  //           // // Write our data back to the cache.
+  //             store.writeQuery({ query:AllPosts, data })
+  //          }  
+
+  //         })
+  //         //  this.isLoading=true;
+  //           .then( response => {
+  //              this.isLoading=false;
+  //             this.$router.push("/sites");
+  //              console.log(response)
+  //           })
 
 
-//          console.log("Data:", result);
-        
-//      },
-   }
-  
+  //          console.log("Data:", result);
+
+  //      },
+     }
+
 }
 
 </script>
