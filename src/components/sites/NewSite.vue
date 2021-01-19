a69632c37a9249d7cbf1bd43b50d6be099a72d12
AssemayImankulova <49196674+AssemayImankulova@users.noreply.github.com>
2021-01-19 20:32:45 +0600

    Update NewSite.vue

--- src/components/sites/NewSite.vue
+++ src/components/sites/NewSite.vue
@@ -4,11 +4,11 @@
        <form>
         <div class="form-group">
             <label>siteName</label>
-            <input  class="form-control"  placeholder="enter siteName">
+            <input  class="form-control"  placeholder="Mangystau">
         </div>
         <div class="form-group">
             <label>siteComments</label>
-            <textarea class="form-control" rows="3" placeholder="enter siteComments"></textarea>
+            <textarea class="form-control" rows="3" placeholder="Desert area"></textarea>
         </div>
        </form>
        <button class="btn btn-outline-primary">Create</button>
